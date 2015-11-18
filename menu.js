4675f228bfdcc420eb5882cb75a3630b682b04cc
Barry Haddow <barry.haddow@gmail.com>
2015-11-18 22:46:07 +0000

    first attempt at factoring out menu

---
+++ menu.js
@@ -0,0 +1,13 @@
+document.write('
+[ <A HREF="index.html">HOME</A>] \
+| [<A HREF="translation-task.html">TRANSLATION TASK</A>] \
+| [<A HREF="metrics-task/">METRICS TASK</A>] \
+| [<A HREF="tuning-task/">TUNING TASK</A>] \
+| [<A HREF="quality-estimation-task.html">QUALITY ESTIMATION TASK</A>] <br> \
+|  [<A HREF="ape-task.html">AUTOMATIC POST-EDITING TASK</A>] \
+|  [<A HREF="program.html">SCHEDULE</A>]  \
+|  [<A HREF="papers.html">PAPERS</A>]  \
+|  [<A HREF="authors.html">AUTHORS</A>] \ 
+|  [<A HREF="results.html">RESULTS</A>]\
+');
+

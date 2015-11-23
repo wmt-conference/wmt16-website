7e74c6091932c8d6a53d5b4bb49b9d163f7a9edb
Barry Haddow <barry.haddow@gmail.com>
2015-11-23 21:23:29 +0000

    update front page for 2016

--- menu.js
+++ menu.js
@@ -1,13 +1,18 @@
 document.write('\
 [ <A HREF="index.html">HOME</A>] \
-| [<A HREF="translation-task.html">TRANSLATION TASK</A>] \
-| [<A HREF="metrics-task/">METRICS TASK</A>] \
-| [<A HREF="tuning-task/">TUNING TASK</A>] \
-| [<A HREF="quality-estimation-task.html">QUALITY ESTIMATION TASK</A>] <br> \
-|  [<A HREF="ape-task.html">AUTOMATIC POST-EDITING TASK</A>] \
-|  [<A HREF="program.html">SCHEDULE</A>]  \
-|  [<A HREF="papers.html">PAPERS</A>]  \
-|  [<A HREF="authors.html">AUTHORS</A>] \
-|  [<A HREF="results.html">RESULTS</A>]\
+| [<A HREF="translation-task.html">NEWS TRANSLATION TASK</A>] \
+| [<A HREF="it-translation-task.html">IT TRANSLATION TASK</A>] \
+| [<A HREF="biomedical-translation-task.html">BIOMEDICAL TRANSLATION TASK</A>] \
+| [<A HREF="ape-task.html">AUTOMATIC POST-EDITING TASK</A>] \
+| [<A HREF="metrics-task.html">METRICS TASK</A>] <br> \
+ [<A HREF="quality-estimation-task.html">QUALITY ESTIMATION TASK</A>]   \
+| [<A HREF="tuning-task.html">TUNING TASK</A>] \
+| [<A HREF="pronoun-task.html">PRONOUN TASK</A>] \
+| [<A HREF="bilingual-task.html">BILINGUAL DOCUMENT ALIGNMENT TASK</A>] \
+| [<A HREF="multimodal-task.html">MULTIMODAL TRANSLATION TASK</A>] <br> \
+| [<A HREF="program.html">SCHEDULE</A>]  \
+| [<A HREF="papers.html">PAPERS</A>]  \
+| [<A HREF="authors.html">AUTHORS</A>] \
+| [<A HREF="results.html">RESULTS</A>]\
 ');
 
